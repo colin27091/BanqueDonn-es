f6a00a6d9b65f89a120e534d3c6998f1014e64c0
colin27091 <colinregourd@gmail.com>
2018-03-28 11:47:40 +0200

    commit2

--- src/Application.java
+++ src/Application.java
@@ -1,15 +1,5 @@
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.GridBagLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
