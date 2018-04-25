0058dccb44f42eeff7f959e263dcb8250e489370
colin27091 <colinregourd@gmail.com>
2018-04-25 23:32:43 +0200

    Liste d'affichage

--- src/PanelCentralHaut.java
+++ src/PanelCentralHaut.java
@@ -1,16 +1,33 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class PanelCentralHaut extends JPanel {
+	
+	String[] affichages = {"Affichage1", "Affichage2", "Affichage3"};
 	
 	private static final long serialVersionUID = 1L;
 
 	PanelCentralHaut(){
 		super();
 		
+		this.setLayout(new BorderLayout());
+		
 		this.setBorder(BorderFactory.createLineBorder(Color.red));
+		
+	
+		JComboBox<String> aff = new JComboBox<String>(affichages);
+		aff.setSelectedIndex(0);
+		
+		this.add(aff, BorderLayout.EAST);
 	}
 
 }
